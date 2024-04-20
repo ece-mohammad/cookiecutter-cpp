ead2e3e4857760b5d23b2c755f22efe1ecc20906
ece-mohammad <kuro.ece@gmail.com>
2024-04-20 04:17:02 +0200

    - Added .vscode workspace directory - Added vscode workspace file - Added CMakePresets.json - Modified CMake configuration options to use ccache - Modified gtest, now it's added as a submodule in th post hook - Modified gtest library build file - Added include directory

---
+++ {{cookiecutter.repo_name}}/include/{{cookiecutter.repo_name|lower}}.hpp
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * @file      {{cookiecutter.repo_name|lower}}.hpp
+ * @brief     {{cookiecutter.description}}
+ * @version   {{cookiecutter.version}}
+ * @date      
+ * @author    {{cookiecutter.author}}
+ * @copyright {{cookiecutter.year}}
+ *****************************************************************************/
+ 
+ #ifndef __{{cookiecutter.repo_name|upper}}__
+ #define __{{cookiecutter.repo_name|upper}}__
+ 
+ 
+ 
+ #endif /* __{{cookiecutter.repo_name|upper}}__ */
--- {{cookiecutter.repo_name}}/test/test_{{cookiecutter.repo_name|lower}}.cpp
+++ {{cookiecutter.repo_name}}/test/test_{{cookiecutter.repo_name|lower}}.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <{{cookiecutter.repo_name.lower()}}.hpp>
 
 TEST({{ cookiecutter.project_name | capitalize }}Test, Contrived) {
     EXPECT_TRUE(true);
