630ad903ddae67af69ee4aff7f33e4f42a1e2e9c
ece-mohammad <kuro.ece@gmail.com>
2024-04-20 04:17:02 +0200

    - Added .vscode workspace directory - Added vscode workspace file - Added CMakePresets.json - Modified CMake configuration options to use ccache - Modified gtest, now it's added as a submodule in th post hook - Modified gtest library build file - Added include directory

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,9 +5,14 @@ if __name__ == '__main__':
     url    = 'git@github.com:{{ cookiecutter.username }}/{{ cookiecutter.repo_name }}'
     branch = '{{ cookiecutter.branch }}'
     remote = '{{ cookiecutter.remote }}'
-
+    gtest_link = 'git@github.com:google/googletest.git'
+    gtest_path = 'external/GTest'
+    
     import subprocess
+    
     subprocess.call(('git', 'init'))
     subprocess.call(('git', 'checkout', '-b', branch))
     subprocess.call(('git', 'remote', 'add', remote, url))
+    subprocess.call(('git', 'submodule', 'add', '--name', 'GTest', '--force', gtest_link, gtest_path))
     subprocess.call(('mv', 'pre-commit', '.git/hooks/'))
+    
